c494ed24c58006e3610622d18e87b70c8552ac3c
Saugat Tripathi <saugat.tripathi@bison.howard.edu>
2016-02-14 02:24:48 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+// 	name:'Andrew',
+// 	age: 21
+// };
+
+// function udatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// udatePerson(person);
+// console.log(person);
+
+
+// Array example
+array = [100, 200, 300, 400];
+function updateArray(array) {
+	array.push(100);
+	debugger;
+	console.log(array);
+}
+
+updateArray(array);
